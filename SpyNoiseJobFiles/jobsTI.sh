31ac16eed77eb5b67ecf12b64e0d46d76be78f75
Bjorn Burkle <bbbjornburkle@gmail.com>
2022-05-25 21:32:41 +0200

    Adding .sh files for running edm configs and setting confdb info

---
+++ SpyNoiseJobFiles/jobsTI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+#cd /afs/cern.ch/work/b/bburkle/public/copy_of_jblee/CMSSW_10_6_0_pre4/src
+cd /afs/cern.ch/work/b/bburkle/public/CMSSW_GitHub/CMSSW_12_1_0/src/DQM
+eval `scramv1 runtime -sh`
+#export CONFDB=cms_trk_r/1A3C5E7G:FIN@cms_omds_lb
+export CONFDB=cms_trk_r/Liwcer_2_JUI@cms_omds_lb
+#export CONFDB=cms_trk_r/Liwcer_2_JUI@cmsonr1
+cmsRun sourcefromedm_TI_27-JAN-2010_2_346439_cfg.py first=$1 last=$2
+
+echo "Done"
